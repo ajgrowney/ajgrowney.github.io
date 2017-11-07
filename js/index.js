dfb76e3b0cc19a6d5f59b2e6d7c323ca220d03b3
Andrew Growney <ajgrowney@ku.edu>
2017-11-07 13:21:05 -0600

    Push buttons functions added

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function techClick(){
+
+}
+function leadClick(){
+
+}
+function involveClick(){
+  
+}
+
+$(document).ready(function(){
+
+  $("#tech").click(function(){
+    techClick();
+  });
+  $("#lead").click(function(){
+    leadClick();
+  });
+  $("#involve").click(function(){
+    involveClick();
+  });
+
+});
