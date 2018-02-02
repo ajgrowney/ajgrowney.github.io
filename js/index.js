2b3269f562f6420ca59fa5a870e865e2aaefb9e8
ajgrowney <ajgrowney@gmail.com>
2018-02-02 10:18:07 -0600

    Mission

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+  $("#main_about").html("\tI have a passion for engineering, people, music, coffee, and faith."+
+"As an undergraduate at the University of Kansas, I am currently studying Interdisciplinary Computing "+
+"with an emphasis in Physics, and finishing a minor in Business Administration.");
 
   $("#resume").click(function(){
   });
