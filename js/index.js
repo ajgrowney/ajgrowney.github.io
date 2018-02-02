ced0eb1ba3a092bb708ae7cbfeaab78d5239092c
ajgrowney <ajgrowney@gmail.com>
2018-02-02 12:15:18 -0600

    Working on links

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 "As an undergraduate at the University of Kansas, I am currently studying Interdisciplinary Computing "+
 "with an emphasis in Physics, and finishing a minor in Business Administration. "+
 "<br><br>As an active member of the SELF Engineering Leadership Fellows, Beta Sigma Psi, the St. Lawrence Center, I have had an amazing opportunity to develop "+
-"leadership, interpersonal, engineering, business, and many other skills. If you are interested in checking out what I've been working on, check out some of the links above! "+
+"leadership, interpersonal, engineering, business, and many other skills. If you are interested in checking out what I've been working on, I am currently working on some of the links above, so please come back very soon!"+
 "<br><br>Thanks for coming in!"
 );
 
