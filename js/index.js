0d437ea0f7128f9f775146cfc3129edd0f7b2789
ajgrowney <ajgrowney@gmail.com>
2018-02-02 12:25:59 -0600

    Grammar

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("#main_about").html("\tI have a passion for high quality engineering, all people, all music, any coffee, and my faith. "+
 "As an undergraduate at the University of Kansas, I am currently studying Interdisciplinary Computing "+
 "with an emphasis in Physics, and finishing a minor in Business Administration. "+
-"<br><br>As an active member of the SELF Engineering Leadership Fellows, Beta Sigma Psi, the St. Lawrence Center, I have had an amazing opportunity to develop "+
+"<br><br>As an active member of the SELF Engineering Leadership Fellows, Beta Sigma Psi, and the St. Lawrence Center, I have had an amazing opportunity to develop "+
 "leadership, interpersonal, engineering, business, and many other skills. If you are interested in checking out what I've been working on, I am currently working on some of the links above, so please come back very soon!"+
 "<br><br>Thanks for coming in!"
 );
