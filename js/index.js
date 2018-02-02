eb11ce642ffa35035b6566fea111f90004a151b1
ajgrowney <ajgrowney@gmail.com>
2018-02-02 11:14:11 -0600

    CSS Gridding

--- js/index.js
+++ js/index.js
@@ -2,7 +2,9 @@ $(document).ready(function(){
   $("#main_about").html("\tI have a passion for high quality engineering, all people, all music, any coffee, and my faith. "+
 "As an undergraduate at the University of Kansas, I am currently studying Interdisciplinary Computing "+
 "with an emphasis in Physics, and finishing a minor in Business Administration. "+
-"<br>"
+"<br><br>As an active member of the SELF Engineering Leadership Fellows, Beta Sigma Psi, the St. Lawrence Center, I have had an amazing opportunity to develop "+
+"leadership, interpersonal, engineering, business, and many other skills. If you are interested in checking out what I've been working on, check out some of the links above! "+
+"<br><br>Thanks for coming in!"
 );
 
   $("#resume").click(function(){
