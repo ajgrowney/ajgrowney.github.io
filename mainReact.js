6a0f91c76ef9746474a72258a523763a50480c02
Andrew Growney <ajgrowney@ku.edu>
2018-08-29 17:30:10 -0500

    Fix: musixapi

---
+++ mainReact.js
@@ -0,0 +1,165 @@
+
+
+class Track extends React.Component{
+    constructor(props){
+        super(props);
+        this.track = this.props.song.track;
+        this.key = this.props.id;
+
+    }
+    componentWillReceiveProps(newProps){
+        this.track = newProps.song.track;
+        this.key = newProps.key;
+    }
+    render(){
+        return(
+            <button onClick={this.props.onClick} className="track-button">
+                {this.track.track_name}
+                <br></br>
+                By: {this.track.artist_name}
+            </button>
+        )
+    }
+}
+
+
+class LyricsDisplay extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            songs: this.props.songs,
+            view: this.props.view
+        }
+        this.handleClick = this.props.handler;
+    }
+
+    componentWillReceiveProps(newProps){
+        this.setState({
+            songs: newProps.songs,
+            view: newProps.view
+        })
+    }
+
+    render(){
+        if(this.state.view == "list"){
+            return(<div>{this.state.songs.map((song) => <Track onClick={() => this.handleClick(song.track.track_id)} key={song.track.track_id} id={song.track.track_id} song={song} />)}</div>)
+        }else if(this.state.view == "track"){
+            return(<div>{this.state.songs}</div>)
+        }else{
+            return(<div></div>)
+        }
+
+    }
+}
+
+class SearchBar extends React.Component {
+    constructor(props){
+        super(props);
+        this.handler = this.props.handler
+    }
+
+    render() {
+        return (
+        <form id="searchArea" onSubmit={this.handler}>
+            <input type="text" ref="artistSearch" placeholder="Type an Artist" />
+            <input value="Get Me My Lyrics" type="submit"/>
+        </form>
+        );
+    }
+}
+
+function GetAPIKey(){
+    return new Promise(function(resolve) {
+        resolve(prompt("Enter your musixmatch api key: "));
+    });
+}
+
+class MainApp extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            songs: [],
+            lyrics_view: undefined
+        }
+
+        GetAPIKey().then((response) => {this.apikey = response});
+    }
+
+
+    makeRequest(track, artist){
+        const self = this;
+        $.ajax({
+            type: "GET",
+            data: {
+                apikey: this.apikey,
+                q_track: track,
+                q_artist: artist,
+                f_has_lyrics: 1,
+                s_artist_rating: "asc",
+                format:"jsonp",
+                callback:"callback"
+            },
+            url: "http://api.musixmatch.com/ws/1.1/track.search",
+            dataType: "jsonp",
+            jsonpCallback: 'callback',
+            contentType: 'application/json',
+            success: function(data) {
+                self.setState({
+                    songs: data.message.body.track_list,
+                    lyrics_view: "list"
+                }) 
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }    
+        });
+    }
+    makeTrackRequest(track_id){
+        const self = this;
+        $.ajax({
+            type: "GET",
+            data: {
+                apikey: this.apikey,
+                track_id,
+                format:"jsonp",
+                callback:"callback"
+            },
+            url: "http://api.musixmatch.com/ws/1.1/track.lyrics.get",
+            dataType: "jsonp",
+            jsonpCallback: 'callback',
+            contentType: 'application/json',
+            success: function(data) {
+                self.setState({
+                    songs: data.message.body.lyrics.lyrics_body,
+                    lyrics_view: "track"
+                }) 
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(jqXHR);
+                console.log(textStatus);
+                console.log(errorThrown);
+            }    
+        });   
+    }
+
+
+
+
+    render() {
+        return (
+            <div>
+                <form action="javascript:void(0)" id="searchArea" onSubmit={() => this.makeRequest(this.refs.trackSearch.value, this.refs.artistSearch.value)}>
+                    <div className="form-element">Song: <input type="text" ref="trackSearch" placeholder="Type a Track" /></div>
+                    <div className="form-element">Artist: <input type="text" ref="artistSearch" placeholder="Type an Artist" /></div>
+                    <br />
+                    <input value="Get Me My Lyrics" type="submit"/>
+                </form>
+                <LyricsDisplay handler={this.makeTrackRequest.bind(this)} view={this.state.lyrics_view} songs={this.state.songs}/>
+            </div>
+        );
+    }
+}
+ReactDOM.render(<MainApp />,document.getElementById('root'));
