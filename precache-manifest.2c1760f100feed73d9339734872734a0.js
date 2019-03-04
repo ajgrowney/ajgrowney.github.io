3692251da5c3471a032117437cd4d740af589a82
Andrew Growney <ajgrowney@gmail.com>
2019-03-04 11:13:43 -0600

    Update: new website

---
+++ precache-manifest.2c1760f100feed73d9339734872734a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "25cccca8b93d7e423f9eee8607cf60dd",
+    "url": "/static/media/pro_pic.25cccca8.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b7acd216a75231891133",
+    "url": "/static/js/main.b7acd216.chunk.js"
+  },
+  {
+    "revision": "43d0e3df44807b3b89ea",
+    "url": "/static/js/2.43d0e3df.chunk.js"
+  },
+  {
+    "revision": "b7acd216a75231891133",
+    "url": "/static/css/main.74b6a031.chunk.css"
+  },
+  {
+    "revision": "43d0e3df44807b3b89ea",
+    "url": "/static/css/2.fc8b926c.chunk.css"
+  },
+  {
+    "revision": "11a5e02075d61bee370385110c08bfe0",
+    "url": "/index.html"
+  }
+];
